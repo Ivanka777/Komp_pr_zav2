af3a7625c155491b91afda636afde456bf6c6336
Ivanka777 <115572638+Ivanka777@users.noreply.github.com>
2024-10-18 12:20:53 +0300

    Add files via upload

---
+++ MyForm.h
@@ -0,0 +1,276 @@
+﻿#pragma once
+
+namespace Project1 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Сводка для MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::TabControl^ tabControl1;
+	protected:
+	private: System::Windows::Forms::TabPage^ tabPage1;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::TabPage^ tabPage2;
+	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::Label^ label6;
+
+	private:
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->tabControl1 = (gcnew System::Windows::Forms::TabControl());
+			this->tabPage1 = (gcnew System::Windows::Forms::TabPage());
+			this->label6 = (gcnew System::Windows::Forms::Label());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->tabPage2 = (gcnew System::Windows::Forms::TabPage());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->tabControl1->SuspendLayout();
+			this->tabPage1->SuspendLayout();
+			this->tabPage2->SuspendLayout();
+			this->SuspendLayout();
+			// 
+			// tabControl1
+			// 
+			this->tabControl1->Controls->Add(this->tabPage1);
+			this->tabControl1->Controls->Add(this->tabPage2);
+			this->tabControl1->Location = System::Drawing::Point(2, -3);
+			this->tabControl1->Name = L"tabControl1";
+			this->tabControl1->SelectedIndex = 0;
+			this->tabControl1->Size = System::Drawing::Size(498, 453);
+			this->tabControl1->TabIndex = 0;
+			// 
+			// tabPage1
+			// 
+			this->tabPage1->BackColor = System::Drawing::Color::LightCyan;
+			this->tabPage1->Controls->Add(this->label6);
+			this->tabPage1->Controls->Add(this->textBox3);
+			this->tabPage1->Controls->Add(this->textBox2);
+			this->tabPage1->Controls->Add(this->textBox1);
+			this->tabPage1->Controls->Add(this->label4);
+			this->tabPage1->Controls->Add(this->label3);
+			this->tabPage1->Controls->Add(this->label2);
+			this->tabPage1->Controls->Add(this->label1);
+			this->tabPage1->Controls->Add(this->button1);
+			this->tabPage1->ForeColor = System::Drawing::SystemColors::ControlText;
+			this->tabPage1->Location = System::Drawing::Point(4, 25);
+			this->tabPage1->Name = L"tabPage1";
+			this->tabPage1->Padding = System::Windows::Forms::Padding(3);
+			this->tabPage1->Size = System::Drawing::Size(490, 424);
+			this->tabPage1->TabIndex = 0;
+			this->tabPage1->Text = L"Завдання";
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei", 10.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label6->Location = System::Drawing::Point(72, 249);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(109, 25);
+			this->label6->TabIndex = 8;
+			this->label6->Text = L"Відповідь:";
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(198, 249);
+			this->textBox3->Multiline = true;
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(170, 68);
+			this->textBox3->TabIndex = 7;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(110, 164);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(90, 22);
+			this->textBox2->TabIndex = 6;
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(110, 95);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(90, 22);
+			this->textBox1->TabIndex = 5;
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->BackColor = System::Drawing::Color::PapayaWhip;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei UI", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label4->Location = System::Drawing::Point(271, 37);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(198, 27);
+			this->label4->TabIndex = 4;
+			this->label4->Text = L"Точність:   e=0,001";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->BackColor = System::Drawing::Color::PapayaWhip;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei UI", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label3->Location = System::Drawing::Point(18, 37);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(233, 27);
+			this->label3->TabIndex = 3;
+			this->label3->Text = L"Рівняння:   x∙lgx-1,2=0";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label2->Location = System::Drawing::Point(47, 164);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(43, 25);
+			this->label2->TabIndex = 2;
+			this->label2->Text = L"b =";
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label1->Location = System::Drawing::Point(47, 95);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(43, 25);
+			this->label1->TabIndex = 1;
+			this->label1->Text = L"a =";
+			// 
+			// button1
+			// 
+			this->button1->BackColor = System::Drawing::Color::LightBlue;
+			this->button1->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button1->Location = System::Drawing::Point(302, 117);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(128, 56);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Обчислити";
+			this->button1->UseVisualStyleBackColor = false;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// tabPage2
+			// 
+			this->tabPage2->BackColor = System::Drawing::Color::LightCyan;
+			this->tabPage2->Controls->Add(this->label5);
+			this->tabPage2->Location = System::Drawing::Point(4, 25);
+			this->tabPage2->Name = L"tabPage2";
+			this->tabPage2->Padding = System::Windows::Forms::Padding(3);
+			this->tabPage2->Size = System::Drawing::Size(490, 424);
+			this->tabPage2->TabIndex = 1;
+			this->tabPage2->Text = L"Автор";
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->BackColor = System::Drawing::Color::PowderBlue;
+			this->label5->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei UI", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label5->Location = System::Drawing::Point(43, 143);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(376, 115);
+			this->label5->TabIndex = 0;
+			this->label5->Text = L"Виконала: Кухта Іванка, Група 4М. \r\n\r\nЗавдання 2: Розв\'язання нелініних рівнянь.\r"
+				L"\n\r\nМетод половинного ділення. Варіант №5";
+			this->label5->Click += gcnew System::EventHandler(this, &MyForm::label5_Click);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(499, 419);
+			this->Controls->Add(this->tabControl1);
+			this->Name = L"MyForm";
+			this->Text = L"Завдання 2";
+			this->tabControl1->ResumeLayout(false);
+			this->tabPage1->ResumeLayout(false);
+			this->tabPage1->PerformLayout();
+			this->tabPage2->ResumeLayout(false);
+			this->tabPage2->PerformLayout();
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private: System::Void label5_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+	// Отримання  значень a і b
+	double a = Convert::ToDouble(textBox1->Text);
+	double b = Convert::ToDouble(textBox2->Text);
+	double tochnist = 0.001;
+
+	// Метод половинного ділення
+	double c;
+	do
+	{
+		c = (a + b) / 2;
+		double f_c = c * Math::Log10(c) - 1.2;
+		if (f_c * (a * Math::Log10(a) - 1.2) < 0)
+		{
+			b = c;
+		}
+		else
+		{
+			a = c;
+		}
+	} while (Math::Abs(b - a) > tochnist);
+	// Виведення результату
+	textBox3->Text = c.ToString();	
+}
+
+};
+}
